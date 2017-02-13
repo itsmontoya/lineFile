782ae4441cbb6c9bde09dc97ee18d51d741d4ad3
itsMontoya <josh@montoya.io>
2017-02-13 11:32:16 -0800

    Update aio backend to new async format

--- backends.go
+++ backends.go
@@ -3,11 +3,8 @@ package lineFile
 import (
 	"os"
 
-	"github.com/itsmontoya/aio"
+	"github.com/itsmontoya/async/file"
 )
-
-// TODO: Figure out a better way to handle initializing aio
-var am *aio.AIO
 
 // getOS is an OpenFile func for the os backend
 func getOS(name string, flag int, perm os.FileMode) (fi FileInt, err error) {
@@ -16,9 +13,5 @@ func getOS(name string, flag int, perm os.FileMode) (fi FileInt, err error) {
 
 // getAIO is an OpenFile func for the aio backend
 func getAIO(name string, flag int, perm os.FileMode) (fi FileInt, err error) {
-	if am == nil {
-		am = aio.New(1)
-	}
-
-	return am.OpenFile(name, flag, perm)
+	return file.OpenFile(name, flag, perm)
 }
